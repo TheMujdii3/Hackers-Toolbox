e69b3a9c966be2724e9d836c2919a7658793ce6d
TheMujdii3 <andreineculai70@gmail.com>
2025-06-18 08:19:23 +0300

    fail save ca imi da emotii gradelu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.datastore.core.android)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
