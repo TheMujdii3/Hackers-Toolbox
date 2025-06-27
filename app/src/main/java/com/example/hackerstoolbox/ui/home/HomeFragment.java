9a51f589ab9025c3512e7eeb88776c26a7c8f59b
TheMujdii3 <72468962+TheMujdii3@users.noreply.github.com>
2025-06-27 11:51:49 +0300

    nu stiu ce fac aici

--- app/src/main/java/com/example/hackerstoolbox/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hackerstoolbox/ui/home/HomeFragment.java
@@ -4,29 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.hackerstoolbox.databinding.WebshellCyberchefBinding;
 import com.example.hackerstoolbox.databinding.FragmentHomeBinding;
+import com.example.hackerstoolbox.ui.Cyberchef.CyberchefViewModel;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private WebshellCyberchefBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+//        CyberchefViewModel galleryViewModel =
+//                new ViewModelProvider(this).get(CyberchefViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        binding = WebshellCyberchefBinding.inflate(inflater, container, false);                       //FragmentGalleryBinding.inflate(inflater, container, false);
+        WebView client = binding.webView;
+        WebSettings webSettings = client.getSettings();
+        String mobileUserAgent = "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+        webSettings.setUserAgentString(mobileUserAgent);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+        client.getSettings().setJavaScriptEnabled(true);
+        client.loadUrl("https://github.com/TheMujdii3");
+        return binding.getRoot();
     }
 
     @Override
